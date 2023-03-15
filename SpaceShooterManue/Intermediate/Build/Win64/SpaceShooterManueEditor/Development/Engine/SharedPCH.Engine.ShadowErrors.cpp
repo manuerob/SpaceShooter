30ba872dfe5584da3821596a38300d43cf61096c
Emmanuelle Roberge <emmanuelle.roberge@polymtl.ca>
2023-03-15 00:44:59 -0400

    ER: Main menu beautiful UI + Directional light behind camera

--- SpaceShooterManue/Intermediate/Build/Win64/SpaceShooterManueEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ SpaceShooterManue/Intermediate/Build/Win64/SpaceShooterManueEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.28.29336
+// Compiler: 14.29.30148
 #include "C:/Users/emman/Desktop/Legacy Mob Game Jams/SpaceShooter/SpaceShooterManue/Intermediate/Build/Win64/SpaceShooterManueEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
